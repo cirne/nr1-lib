844a33dfa07a9c3374d0c3cbe901625c2abd5d6d
Lew Cirne <cirne@newrelic.com>
2019-09-10 13:54:27 -0600

    color spectrum tweaks

--- components/heat-map.js
+++ components/heat-map.js
@@ -215,7 +215,7 @@ function prepare({data, max}) {
 function heatMapColor(value) {
   if(value > 1) throw "heatMapColor: value must be in range (0..1)"
 
-  const h = (1 - value) * 80
+  const h = (1 - value) * 70+20
   const s = 100
   const l = 40
 
